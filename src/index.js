140d8bdd914d36a38a7667017994bcb730e7dab6
almber <almaberenice123@gmail.com>
2023-05-09 17:31:09 -0600

    ci: revisar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import { ParagraphLink } from './components/paragraphLink/ParagraphLink';
-import P from './components/paragraphLink/ParagraphLink';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render( 
   <React.StrictMode>
-     {/* <p href="http"> texto</p> */}
-     <P href="http://legacy.reactjs.org/" color= "red" >Página React</P>
-     <P href="httpa://youtube.com" color="green" >Página Youtube</P>
-     <P href="https://linkedin.com" color="purple" >Página LinkedIn</P>
-     <P href="https://linkedin.com" >Página Generation</P>
-     <P href="#">Página CH26</P>
+    < App />
   </React.StrictMode>
 );
 
