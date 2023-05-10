b2b62557df4eaa2faef750440c0f8e65fc9a4594
almber <almaberenice123@gmail.com>
2023-05-10 10:40:39 -0600

    ci: botones

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Counter = () => {
+    let counter = 100;
+
+    const handleIncrement = (event)=>{
+        console.log(event);
+        console.log(++counter);
+    }
+
+  return (
+     <>
+       <h1>Counter</h1>
+       <h2> { counter } </h2>
+       <button onClick={ handleIncrement } >+</button>
+       <button onClick={ (event)=>console.log(event) } >-</button>
+       <button>Reset</button>
+     </>
+  );
+}
